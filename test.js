01e2654703a8986551eb18a834e1dfe2b0564b45
khatwanikunal-a11y <khatwanikunal@gmail.com>
2026-01-23 13:32:55 +0000

    sumOfMultiple

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
 alert("Hello World kk test!");
+
+function sumOfMultiple(f, m) 
+{
+  let sum = 0; 
+
+  for (let i = 0; i < m.length; i++) 
+{
+    let x = m[i]; 
+
+    for (let j = 0; j < f.length; j++) 
+{
+      let d = f[j]; 
+
+      if (d !== 0 && x % d === 0) 
+{
+        sum += x;   
+        break;      
+      }
+    }
+  }
+
+  return sum;
+}
